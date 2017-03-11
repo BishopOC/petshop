718f01772f1eac79a887a8b2fdfc0cd746eed272
Bishop O'Connell <bishopoconnell1@gmail.com>
2017-03-10 19:27:42 -0500

    successfully implemented the delete route

--- index.js
+++ index.js
@@ -81,6 +81,19 @@ server.post('/animals', function(req, res){
 });
 // PUT /animals/:id
 // DELETE /animals:id
+server.delete('/animals/:id', function(req, res){
+  Animal.remove({_id: req.params.id}, function(err, doc){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        msg: 'Successfully deleted'
+      });
+    }
+  });
+});
 
 server.listen(port, function(){
   console.log('Now listening on port...', port);
